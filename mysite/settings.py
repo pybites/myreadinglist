f7e521092f44c70d6859ca1a2ef95f7b5f69db1c
bbelderbos <bobbelderbos@gmail.com>
2017-08-25 14:51:24 +0200

    disable whitenoise for now

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,9 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-print(PROJECT_ROOT)
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
