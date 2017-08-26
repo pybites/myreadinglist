a78ea5efb9b45b2227b687fb7f382d148abcfdf7
bbelderbos <bobbelderbos@gmail.com>
2017-08-26 03:21:06 +0200

    navbar

--- mysite/static/js/base.js
+++ mysite/static/js/base.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-    $("#commentForm").hide();
-
     $("#searchTitles").autocomplete( "/api/get_books/");  
     
     // http://forum.jquery.com/topic/jquery-autocomplete-submit-form-on-result
@@ -17,20 +15,4 @@ $(document).ready(function(){
             }
     });
 
-    // http://www.dailycoding.com/Posts/default_text_fields_using_simple_jquery_trick.aspx
-    $(".defaultText").focus(function(srcc){
-        if ($(this).val() == $(this)[0].title){
-            $(this).removeClass("defaultTextActive");
-            $(this).val("");
-        }
-    });
-
-    $(".defaultText").blur(function(){
-        if ($(this).val() == ""){
-            $(this).addClass("defaultTextActive");
-            $(this).val($(this)[0].title);
-        }
-    });
-
-    $(".defaultText").blur();
 });
